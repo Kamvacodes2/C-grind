744fce9d90bb4573f6f8de86b8e1151f12193359
Kamvacodes2 <mtikasoga96@gmail.com>
2025-12-26 16:01:03 +0200

    Exercise 3 conditional

--- Exercise_3/Exercise_3/Program.cs
+++ Exercise_3/Exercise_3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TallandName
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isMale = false;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("This is a male and he is tall");
+            }
+            else if (isMale)
+            {
+                Console.WriteLine("He short");
+            }
+            else if(isTall)
+            {
+                Console.WriteLine("She is tall");
+            }
+            else
+            {
+                Console.WriteLine("She is short");
+            }
+        }
+    }
+}
