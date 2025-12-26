d66895d8b0005d05f73de8c79d54513fd5d57356
Kamvacodes2 <mtikasoga96@gmail.com>
2025-12-26 16:38:48 +0200

    Exercise 5 Conversions and inputs

---
+++ Exercise_5/Exercise_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the first number ");
+            double firstNumb = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Kindly enter the second number ");
+            double secondNumb = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(firstNumb + secondNumb);
+        }
+    }
+}
