236108b5abcb06b0558c953c35d17c0a3bed6398
Kamvacodes2 <mtikasoga96@gmail.com>
2025-12-26 15:50:23 +0200

    Exercise 2

---
+++ Exercise_2/Exercise_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BoolChecks
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Siphindile";
+            int age = 30;
+            string nameAge = ("This is " + name + " she is " + age + " years old.");
+
+            Console.WriteLine(nameAge);
+;        }
+
+    }
+}
