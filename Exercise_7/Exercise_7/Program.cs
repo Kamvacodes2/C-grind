d82648660aaf6ae67fbc1e255325b372682976fe
Kamvacodes2 <mtikasoga96@gmail.com>
2025-12-27 18:47:00 +0200

    Dealing with arrays

---
+++ Exercise_7/Exercise_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] luckyNumbers = { 4, 8, 90, 55, 45, 30, 2 };
+            luckyNumbers[2] = 1000;
+
+            Console.WriteLine(luckyNumbers[0]);
+            Console.WriteLine(luckyNumbers[2]);
+            Console.WriteLine(luckyNumbers[1]);
+        }
+    }
+}
